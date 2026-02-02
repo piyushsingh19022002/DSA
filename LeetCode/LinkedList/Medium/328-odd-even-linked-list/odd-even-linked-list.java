4ec138a253735354933966a1a5ceb6e49a3c4d44
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-02 10:15:01 +0530

    Time: 1 ms (8.44%) | Memory: 46.8 MB (11.99%) - LeetSync

--- LeetCode/LinkedList/Medium/328-odd-even-linked-list/odd-even-linked-list.java
+++ LeetCode/LinkedList/Medium/328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,25 +10,46 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        // ListNode temp = head;
+        // ArrayList<Integer> data = new ArrayList<>();
+        // while(temp!=null){
+        //     data.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // for(int i = 1 ; i <data.size();i++){
+        //     if(i%2==0){
+        //         temp.next = new ListNode(data.get(i));
+        //         temp = temp.next;
+        //     }
+        // }
+        // for(int i = 1 ; i <data.size();i++){
+        //     if(i%2!=0){
+        //         temp.next = new ListNode(data.get(i));
+        //         temp = temp.next;
+        //     }
+        // }
+        // return head;
+        if(head==null || head.next==null) return head;
+        ListNode oddDummy = new ListNode(-1);
+        ListNode oddTemp = oddDummy;
+        ListNode evenDummy = new ListNode(-1);
+        ListNode evenTemp = evenDummy;
         ListNode temp = head;
-        ArrayList<Integer> data = new ArrayList<>();
+        int idx = 0;
         while(temp!=null){
-            data.add(temp.val);
+            if(idx%2==0){
+                oddTemp.next = new ListNode(temp.val);
+                oddTemp = oddTemp.next;
+                idx++;
+            }else{
+                evenTemp.next = new ListNode(temp.val);
+                evenTemp = evenTemp.next;
+                idx++;
+            }
             temp = temp.next;
         }
-        temp = head;
-        for(int i = 1 ; i <data.size();i++){
-            if(i%2==0){
-                temp.next = new ListNode(data.get(i));
-                temp = temp.next;
-            }
-        }
-        for(int i = 1 ; i <data.size();i++){
-            if(i%2!=0){
-                temp.next = new ListNode(data.get(i));
-                temp = temp.next;
-            }
-        }
-        return head;
+        oddTemp.next = evenDummy.next;
+        return oddDummy.next;
     }
 }
