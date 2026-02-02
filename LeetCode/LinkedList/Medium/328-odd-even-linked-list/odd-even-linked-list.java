d724df367cfd9c2bb08d9fa40ba0b82461c30ab0
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-02 10:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (69.88%) - LeetSync

--- LeetCode/LinkedList/Medium/328-odd-even-linked-list/odd-even-linked-list.java
+++ LeetCode/LinkedList/Medium/328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        // BRUTE FORCE -> USING EXTRA ARRAYLIST
         // ListNode temp = head;
         // ArrayList<Integer> data = new ArrayList<>();
         // while(temp!=null){
@@ -30,26 +31,41 @@ class Solution {
         //     }
         // }
         // return head;
+        // BETTER -> USING TWO NEW LL
+        // if(head==null || head.next==null) return head;
+        // ListNode oddDummy = new ListNode(-1);
+        // ListNode oddTemp = oddDummy;
+        // ListNode evenDummy = new ListNode(-1);
+        // ListNode evenTemp = evenDummy;
+        // ListNode temp = head;
+        // int idx = 0;
+        // while(temp!=null){
+        //     if(idx%2==0){
+        //         oddTemp.next = new ListNode(temp.val);
+        //         oddTemp = oddTemp.next;
+        //         idx++;
+        //     }else{
+        //         evenTemp.next = new ListNode(temp.val);
+        //         evenTemp = evenTemp.next;
+        //         idx++;
+        //     }
+        //     temp = temp.next;
+        // }
+        // oddTemp.next = evenDummy.next;
+        // return oddDummy.next;
+        // OPTIMIZE -> BY CHANGING LINKS
         if(head==null || head.next==null) return head;
-        ListNode oddDummy = new ListNode(-1);
-        ListNode oddTemp = oddDummy;
-        ListNode evenDummy = new ListNode(-1);
-        ListNode evenTemp = evenDummy;
-        ListNode temp = head;
-        int idx = 0;
-        while(temp!=null){
-            if(idx%2==0){
-                oddTemp.next = new ListNode(temp.val);
-                oddTemp = oddTemp.next;
-                idx++;
-            }else{
-                evenTemp.next = new ListNode(temp.val);
-                evenTemp = evenTemp.next;
-                idx++;
-            }
-            temp = temp.next;
+        ListNode odd = head;
+        ListNode even = odd.next;
+        ListNode evenHead = even;
+        while(even!=null && even.next!=null){
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
         }
-        oddTemp.next = evenDummy.next;
-        return oddDummy.next;
+        odd.next = evenHead;
+        return head;
     }
 }
