eab33a252bf3eaaaab1e0910f6fe13cd3e17dd4c
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-10 10:10:13 +0530

    Time: 3 ms (99.68%) | Memory: 94.2 MB (89.02%) - LeetSync

--- LeetCode/Stack/Easy/234-palindrome-linked-list/palindrome-linked-list.java
+++ LeetCode/Stack/Easy/234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,47 +10,48 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        boolean flag = true;
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while(temp!=null){
-            st.push(temp.val);
-            temp = temp.next;
-        }
-        temp = head;
-        while(!st.isEmpty()){
-            if(temp.val!=st.pop()){
-                flag = false;
-                break;
-            }else{
-                temp = temp.next;
-            }
-        }
-        return flag;
+        //Using Stack
+        // boolean flag = true;
+        // Stack<Integer> st = new Stack<>();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // while(!st.isEmpty()){
+        //     if(temp.val!=st.pop()){
+        //         flag = false;
+        //         break;
+        //     }else{
+        //         temp = temp.next;
+        //     }
+        // }
+        // return flag;
 
 
-        // ListNode slow = head;
-        // ListNode fast = head;
-        // while(fast!=null && fast.next!=null){
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-        // }
-        // ListNode prev = null;
-        // ListNode curr = slow;
-        // while(curr!=null){
-        //     ListNode next = curr.next;
-        //     curr.next = prev;
-        //     prev = curr;
-        //     curr = next;
-        // }
-        // ListNode i = head;
-        // ListNode j = prev;
-        // while(j!=null){
-        //     if(i.val!=j.val) return false;
-        //     i = i.next;
-        //     j = j.next;
-        // }
-        // return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null;
+        ListNode curr = slow;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode i = head;
+        ListNode j = prev;
+        while(j!=null){
+            if(i.val!=j.val) return false;
+            i = i.next;
+            j = j.next;
+        }
+        return true;
         // ArrayList cpy = new ArrayList<>();
         // ListNode temp = head;
         // while(temp!=null){
