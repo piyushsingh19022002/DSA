e8ff20385e88d2a8f01aee8cd5111d246fdffd49
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-12 20:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (49.15%) - LeetSync

--- LeetCode/BinaryTree/Easy/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ LeetCode/BinaryTree/Easy/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -28,16 +28,31 @@
 //     }
 // }
 
+// class Solution {
+//     public void preorder(TreeNode root,List<Integer> ans){
+//         if(root==null) return;
+//         ans.add(root.val);
+//         preorder(root.left,ans);
+//         preorder(root.right,ans);
+//     }
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         preorder(root,ans);
+//         return ans;
+//     }
+// }
+
 class Solution {
-    public void preorder(TreeNode root,List<Integer> ans){
-        if(root==null) return;
-        ans.add(root.val);
-        preorder(root.left,ans);
-        preorder(root.right,ans);
-    }
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        preorder(root,ans);
+        Stack<TreeNode> st = new Stack<>();
+        if(root!=null) st.push(root);
+        while(!st.isEmpty()){
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if(top.right!=null) st.push(top.right);
+            if(top.left!=null) st.push(top.left);
+        }
         return ans;
     }
 }
