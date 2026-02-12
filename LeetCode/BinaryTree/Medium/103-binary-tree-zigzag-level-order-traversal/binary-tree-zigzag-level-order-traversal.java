383d144dc834ac5a3413b7ae71215b1671d6f30a
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-12 13:38:21 +0530

    Time: 1 ms (81.25%) | Memory: 43.8 MB (40.19%) - LeetSync

--- LeetCode/BinaryTree/Medium/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ LeetCode/BinaryTree/Medium/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -13,34 +13,73 @@
  *     }
  * }
  */
-class Solution {
+// class Solution { // Approach 1 -> Recursion // most optimized
+    // public int levels(TreeNode root){
+    //     if(root==null) return 0;
+    //     return 1 + Math.max(levels(root.left),levels(root.right));
+    // }
+//     public void nthLevelLtoR(TreeNode root,int currLevel,int targetLevel,List<Integer> arr){
+//         if(root==null) return;
+//         if(currLevel==targetLevel) arr.add(root.val);
+//         nthLevelLtoR(root.left,currLevel+1,targetLevel,arr);
+//         nthLevelLtoR(root.right,currLevel+1,targetLevel,arr);
+//     }
+//     public void nthLevelRtoL(TreeNode root,int currLevel,int targetLevel,List<Integer> arr){
+//         if(root==null) return;
+//         if(currLevel==targetLevel) arr.add(root.val);
+//         nthLevelRtoL(root.right,currLevel+1,targetLevel,arr);
+//         nthLevelRtoL(root.left,currLevel+1,targetLevel,arr);
+//     }
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         int lvl = levels(root);
+//         for(int i = 0 ; i < lvl ;i++){
+//             List<Integer> arr = new ArrayList<>();
+//             if(i%2==0){
+//                 nthLevelLtoR(root,0,i,arr);
+//             }else{
+//                 nthLevelRtoL(root,0,i,arr);
+//             }
+//             ans.add(arr);
+//         }
+//         return ans;
+//     }
+// }
+
+class Solution { // Approach 1 -> Using Queue
+    public class Pair{
+        TreeNode node;
+        int level;
+        Pair(TreeNode n , int l){
+            node = n;
+            level = l;
+        }
+    }
     public int levels(TreeNode root){
         if(root==null) return 0;
         return 1 + Math.max(levels(root.left),levels(root.right));
     }
-    public void nthLevelLtoR(TreeNode root,int currLevel,int targetLevel,List<Integer> arr){
-        if(root==null) return;
-        if(currLevel==targetLevel) arr.add(root.val);
-        nthLevelLtoR(root.left,currLevel+1,targetLevel,arr);
-        nthLevelLtoR(root.right,currLevel+1,targetLevel,arr);
-    }
-    public void nthLevelRtoL(TreeNode root,int currLevel,int targetLevel,List<Integer> arr){
-        if(root==null) return;
-        if(currLevel==targetLevel) arr.add(root.val);
-        nthLevelRtoL(root.right,currLevel+1,targetLevel,arr);
-        nthLevelRtoL(root.left,currLevel+1,targetLevel,arr);
-    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        int lvl = levels(root);
-        for(int i = 0 ; i < lvl ;i++){
+        int lvls = levels(root);
+        for(int i = 0 ; i < lvls; i++){
             List<Integer> arr = new ArrayList<>();
-            if(i%2==0){
-                nthLevelLtoR(root,0,i,arr);
-            }else{
-                nthLevelRtoL(root,0,i,arr);
-            }
             ans.add(arr);
+        }
+        Queue<Pair> q = new LinkedList<>();
+        if(root!=null) q.add(new Pair(root,0));
+        while(q.size()>0){
+            Pair front = q.remove();
+            TreeNode temp = front.node;
+            int lvl = front.level;
+            ans.get(lvl).add(temp.val);
+            if(temp.left!=null) q.add(new Pair(temp.left,lvl+1));
+            if(temp.right!=null) q.add(new Pair(temp.right,lvl+1));
+        }
+        for(int i = 0; i < lvls;i++){
+            if(i%2!=0){
+                Collections.reverse(ans.get(i));
+            }
         }
         return ans;
     }
