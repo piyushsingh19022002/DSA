09c397f8b6982551416d3a50e96c0ef2db0532de
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-12 21:42:53 +0530

    Time: 1 ms (7.10%) | Memory: 43.5 MB (12.56%) - LeetSync

--- LeetCode/BinaryTree/Easy/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ LeetCode/BinaryTree/Easy/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -13,16 +13,38 @@
  *     }
  * }
  */
+// class Solution {
+//     public void helper(TreeNode root,List<Integer> ans){
+//         if(root==null) return;
+//         helper(root.left,ans);
+//         ans.add(root.val);
+//         helper(root.right,ans);
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         helper(root,ans);
+//         return ans;
+//     }
+// }
+
 class Solution {
-    public void helper(TreeNode root,List<Integer> ans){
-        if(root==null) return;
-        helper(root.left,ans);
-        ans.add(root.val);
-        helper(root.right,ans);
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        helper(root,ans);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode temp = root;
+        while(true){
+            if(temp!=null){
+                st.push(temp);
+                temp = temp.left;
+            }else{
+                if(st.isEmpty()) break;
+                else{
+                    TreeNode top = st.pop();
+                    ans.add(top.val);
+                    temp = top.right;
+                }
+            }
+        }
         return ans;
     }
 }
