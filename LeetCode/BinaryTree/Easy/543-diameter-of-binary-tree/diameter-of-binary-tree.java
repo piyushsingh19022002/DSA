2d7fd8ef8e7ccaf6515460c73cbc4a06decf872d
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-13 00:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (16.24%) - LeetSync

--- LeetCode/BinaryTree/Easy/543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ LeetCode/BinaryTree/Easy/543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,16 +13,31 @@
  *     }
  * }
  */
+// class Solution {
+//     public int level(TreeNode root){
+//         if(root==null) return 0;
+//         return 1 + Math.max(level(root.left),level(root.right));
+//     }
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         if(root==null) return 0;
+//         int currDia = level(root.left) + level(root.right) + 1 - 1; // h = l - 1;
+//         int leftDia = diameterOfBinaryTree(root.left); 
+//         int rightDia = diameterOfBinaryTree(root.right);
+//         return Math.max(currDia,Math.max(leftDia,rightDia));
+//     }
+// }
 class Solution {
-    public int level(TreeNode root){
+    public int level(TreeNode root,int[] ans){
         if(root==null) return 0;
-        return 1 + Math.max(level(root.left),level(root.right));
+        int rightLevel = level(root.right,ans);
+        int leftLevel = level(root.left,ans);
+        int currDia = rightLevel + leftLevel + 1 - 1;
+        ans[0] = Math.max(ans[0],currDia);
+        return 1 + Math.max(rightLevel,leftLevel);
     }
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null) return 0;
-        int currDia = level(root.left) + level(root.right) + 1 - 1; // h = l - 1;
-        int leftDia = diameterOfBinaryTree(root.left); 
-        int rightDia = diameterOfBinaryTree(root.right);
-        return Math.max(currDia,Math.max(leftDia,rightDia));
+        int[] ans = {0};
+        level(root,ans);
+        return ans[0];
     }
 }
