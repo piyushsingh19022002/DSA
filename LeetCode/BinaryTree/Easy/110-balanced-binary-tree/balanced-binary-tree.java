321932beb6747aff0b1ce1920d3ec4cb8c7bad2d
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-12 23:31:36 +0530

    Time: 1 ms (26.09%) | Memory: 45.7 MB (60.47%) - LeetSync

---
+++ LeetCode/BinaryTree/Easy/110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ // wrong
+// class Solution {
+//     public int levels(TreeNode root){
+//         if(root==null) return 0;
+//         return 1 + Math.max(levels(root.left),levels(root.right));
+//     }
+//     public void dfs(TreeNode root,boolean ans){
+//         if(root==null) return;
+//         if(Math.abs(levels(root.left)-levels(root.right))>1){
+//             ans = false;
+//             return;
+//         }
+//         dfs(root.left,ans);
+//         dfs(root.right,ans);
+//     }
+//     public boolean isBalanced(TreeNode root) {
+//         boolean ans = true;
+//         dfs(root,ans);
+//         return ans;
+//     }
+// }
+class Solution {
+    public int levels(TreeNode root){
+        if(root==null) return 0;
+        return 1 + Math.max(levels(root.left),levels(root.right));
+    }
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true;
+        int diff = Math.abs(levels(root.left)-levels(root.right));
+        if(diff>1) return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
