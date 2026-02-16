5cdeb76c6238b14857d738d2b151d07cd061556c
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-16 21:49:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (26.62%) - LeetSync

---
+++ LeetCode/BinarySearchTree/Medium/784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Solution {
+    public void insert(TreeNode root,int val){
+        if(val > root.val){
+            if(root.right==null) root.right = new TreeNode(val);
+            else insert(root.right,val);
+        }else{
+            if(root.left==null) root.left = new TreeNode(val);
+            else insert(root.left,val);
+        }
+    }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null) return new TreeNode(val); // agar root hi null to new node banakar return
+        insert(root,val);
+        return root;
+    }
+}
+// class Solution {
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+//         if(root==null){
+//             return new TreeNode(val);
+//         }
+//         if(val>root.val){
+//             root.right = insertIntoBST(root.right,val);
+//         }
+//         if(val<root.val){
+//             root.left = insertIntoBST(root.left,val);
+//         }
+//         return root;
+//     }
+// }
