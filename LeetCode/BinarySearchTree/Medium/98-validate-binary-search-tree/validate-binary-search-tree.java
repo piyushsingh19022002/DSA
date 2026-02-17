a86604ee8dce9e73201577c56132ebc06d01f0d2
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-17 19:12:53 +0530

    Time: 2 ms (13.74%) | Memory: 45.6 MB (10.93%) - LeetSync

--- LeetCode/BinarySearchTree/Medium/98-validate-binary-search-tree/validate-binary-search-tree.java
+++ LeetCode/BinarySearchTree/Medium/98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,23 +13,42 @@
  *     }
  * }
  */
+// class Solution {
+//     public long max(TreeNode root){
+//         if(root==null) return Long.MIN_VALUE;
+//         long left = max(root.left);
+//         long right = max(root.right);
+//         return Math.max(root.val,Math.max(left,right));
+//     }
+//     public long min(TreeNode root){
+//         if(root==null) return Long.MAX_VALUE;
+//         long left = min(root.left);
+//         long right = min(root.right);
+//         return Math.min(root.val,Math.min(left,right));
+//     }
+//     public boolean isValidBST(TreeNode root) {
+//         if(root==null) return true;
+//         if(root.val<=max(root.left)) return false;
+//         if(root.val>=min(root.right)) return false;
+//         return isValidBST(root.left) && isValidBST(root.right);
+//     }
+// }
 class Solution {
-    public long max(TreeNode root){
-        if(root==null) return Long.MIN_VALUE;
-        long left = max(root.left);
-        long right = max(root.right);
-        return Math.max(root.val,Math.max(left,right));
+    public boolean isSorted(List<Integer> in){
+        for(int i = 1;i < in.size();i++){
+            if(in.get(i)<=in.get(i-1)) return false;
+        }
+        return true;
     }
-    public long min(TreeNode root){
-        if(root==null) return Long.MAX_VALUE;
-        long left = min(root.left);
-        long right = min(root.right);
-        return Math.min(root.val,Math.min(left,right));
+    public void inorder(TreeNode root,List<Integer> in){
+        if(root==null) return;
+        inorder(root.left,in);
+        in.add(root.val);
+        inorder(root.right,in);
     }
     public boolean isValidBST(TreeNode root) {
-        if(root==null) return true;
-        if(root.val<=max(root.left)) return false;
-        if(root.val>=min(root.right)) return false;
-        return isValidBST(root.left) && isValidBST(root.right);
+        List<Integer> in = new ArrayList<>();
+        inorder(root,in);
+        return isSorted(in);
     }
 }
