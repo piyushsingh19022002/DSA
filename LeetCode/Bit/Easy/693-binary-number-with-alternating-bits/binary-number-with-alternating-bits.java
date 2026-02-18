f7b215b486c094c7394815d1f0c074f9a0795844
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-18 14:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (16.11%) - LeetSync

--- LeetCode/Bit/Easy/693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
+++ LeetCode/Bit/Easy/693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
@@ -1,14 +1,14 @@
 class Solution {
-    public String toBinary(int n){
-        StringBuilder ans = new StringBuilder();
-        while(n>0){
-            ans.append(n%2);
-            n/=2;
-        }
-        return ans.reverse().toString();
-    }
+    // public String toBinary(int n){
+    //     StringBuilder ans = new StringBuilder();
+    //     while(n>0){
+    //         ans.append(n%2);
+    //         n/=2;
+    //     }
+    //     return ans.reverse().toString();
+    // }
     public boolean hasAlternatingBits(int n) {
-        String binary = toBinary(n);
+        String binary = Integer.toBinaryString(n);
         if(binary.length()==1) return true;
         for(int i = 0 ; i < binary.length()-1;i++){
             if(binary.charAt(i)==binary.charAt(i+1)) return false;
