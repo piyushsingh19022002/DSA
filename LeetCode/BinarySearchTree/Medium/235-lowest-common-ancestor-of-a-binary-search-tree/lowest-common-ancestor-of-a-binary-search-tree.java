a7d13c350475f37ae453710e3af88035d4967025
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-18 09:36:28 +0530

    Time: 6 ms (97.06%) | Memory: 47.4 MB (93.66%) - LeetSync

--- LeetCode/BinarySearchTree/Medium/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ LeetCode/BinarySearchTree/Medium/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,9 +1,17 @@
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root.val==p.val || root.val==q.val) return root; // ya to root ho p ya q
+//         if(root.val>q.val && root.val<p.val) return root; // p right me or q left me
+//         if(root.val<q.val && root.val>p.val) return root; // q right me or p left me
+//         if(p.val<root.val && q.val<root.val) return lowestCommonAncestor(root.left,p,q);
+//         else return lowestCommonAncestor(root.right,p,q);
+//     }
+// }
+
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val==p.val || root.val==q.val) return root;
-        if(root.val>q.val && root.val<p.val) return root;
-        if(root.val<q.val && root.val>p.val) return root;
         if(p.val<root.val && q.val<root.val) return lowestCommonAncestor(root.left,p,q);
-        else return lowestCommonAncestor(root.right,p,q);
+        else if(p.val>root.val && q.val>root.val) return lowestCommonAncestor(root.right,p,q);
+        else return root;
     }
 }
