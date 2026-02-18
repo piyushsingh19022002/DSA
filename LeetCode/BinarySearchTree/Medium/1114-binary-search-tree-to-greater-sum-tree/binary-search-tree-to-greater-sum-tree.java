785edb0ebb815c47c51b4adaff3587210adbbad3
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-18 10:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (94.24%) - LeetSync

--- LeetCode/BinarySearchTree/Medium/1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
+++ LeetCode/BinarySearchTree/Medium/1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -13,19 +13,33 @@
  *     }
  * }
  */
+// class Solution {
+//     public void inorder(TreeNode root,List<TreeNode> ans){
+//         if(root==null) return;
+//         inorder(root.left,ans);
+//         ans.add(root);
+//         inorder(root.right,ans);
+//     }
+//     public TreeNode bstToGst(TreeNode root) {
+//         List<TreeNode> ans = new ArrayList<>();
+//         inorder(root,ans);
+//         for(int i = ans.size()-2;i>=0;i--){
+//             ans.get(i).val+=ans.get(i+1).val;
+//         }
+//         return root;
+//     }
+// }
 class Solution {
-    public void inorder(TreeNode root,List<TreeNode> ans){
+    int sum = 0;
+    public void inorder(TreeNode root){
         if(root==null) return;
-        inorder(root.left,ans);
-        ans.add(root);
-        inorder(root.right,ans);
+        inorder(root.right);
+        root.val +=sum;
+        sum = root.val;
+        inorder(root.left);
     }
     public TreeNode bstToGst(TreeNode root) {
-        List<TreeNode> ans = new ArrayList<>();
-        inorder(root,ans);
-        for(int i = ans.size()-2;i>=0;i--){
-            ans.get(i).val+=ans.get(i+1).val;
-        }
+        inorder(root);
         return root;
     }
 }
