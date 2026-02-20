b224355afc5d4f6228e498df06957c9dfe9a0074
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-20 22:46:06 +0530

    Time: 3 ms (84.84%) | Memory: 45.1 MB (73.49%) - LeetSync

--- LeetCode/HashSetAndHashMap/Easy/2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
+++ LeetCode/HashSetAndHashMap/Easy/2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
@@ -1,3 +1,4 @@
+// this will works for both distinct and non distinct
 class Solution {
     public int maximumNumberOfStringPairs(String[] words) {
         int pairs = 0;
@@ -14,3 +15,19 @@ class Solution {
         return pairs;
     }
 }
+// this will work for distinct only
+// class Solution {
+//     public int maximumNumberOfStringPairs(String[] words) {
+//         int pairs = 0;
+//         HashSet<String> set = new HashSet<>();
+//         for(int i = 0 ; i < words.length;i++){
+//             StringBuilder sb = new StringBuilder(words[i]);
+//             if(set.contains(sb.reverse().toString())){
+//                 pairs++;
+//             }
+//             else set.add(words[i]);
+//         }
+        
+//         return pairs;
+//     }
+// }
