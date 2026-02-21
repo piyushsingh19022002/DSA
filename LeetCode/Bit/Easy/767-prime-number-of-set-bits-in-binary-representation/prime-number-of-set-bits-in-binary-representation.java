04e23bee6b45b9d2a3685ac3e10a100fbf04a302
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-21 11:02:23 +0530

    Time: 6 ms (54.33%) | Memory: 44.4 MB (11.21%) - LeetSync

--- LeetCode/Bit/Easy/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ LeetCode/Bit/Easy/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,27 +1,3 @@
-class Solution {
-    public int countSetBits(int n){
-        int ans = 0;
-        String bit = Integer.toBinaryString(n);
-        for(int i = 0 ; i < bit.length();i++){
-            if(bit.charAt(i)=='1') ans++;
-        }
-        return ans;
-    }
-    public boolean isPrime(int val){
-        if(val<=1) return false;
-        for(int i = 2; i <=Math.sqrt(val);i++){
-            if(val%i==0) return false;
-        }
-        return true;
-    }
-    public int countPrimeSetBits(int left, int right) {
-        int primes = 0;
-        for(int i = left; i <=right;i++){
-            if(isPrime(countSetBits(i))) primes++;
-        }
-        return primes;
-    }
-}
 // class Solution {
 //     public int countSetBits(int n){
 //         int ans = 0;
@@ -40,10 +16,26 @@ class Solution {
 //     }
 //     public int countPrimeSetBits(int left, int right) {
 //         int primes = 0;
-//         int[] setBits = new int[right-left+1];
 //         for(int i = left; i <=right;i++){
 //             if(isPrime(countSetBits(i))) primes++;
 //         }
 //         return primes;
 //     }
 // }
+class Solution {
+    public boolean isPrime(int val){
+        if(val<=1) return false;
+        for(int i = 2; i <=Math.sqrt(val);i++){
+            if(val%i==0) return false;
+        }
+        return true;
+    }
+    public int countPrimeSetBits(int left, int right) {
+        int primes = 0;
+        int[] setBits = new int[right-left+1];
+        for(int i = left; i <=right;i++){
+            if(isPrime(Integer.bitCount(i))) primes++;
+        }
+        return primes;
+    }
+}
