45dad0a3cd473feeb34c857cd854970d60447606
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-23 00:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (13.50%) - LeetSync

---
+++ LeetCode/DynamicProgramming/Easy/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+    }
+    public int helper(int n,int[] dp){
+        if(n<=1) return n;
+        if(n==2) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
+    }
+}
