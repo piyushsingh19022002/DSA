d98cdb21d2e72754d2d080a290097fa7e6af20f1
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-22 22:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (86.00%) - LeetSync

--- LeetCode/DynamicProgramming/Easy/1013-fibonacci-number/fibonacci-number.java
+++ LeetCode/DynamicProgramming/Easy/1013-fibonacci-number/fibonacci-number.java
@@ -13,18 +13,31 @@
 //         return cur;
 //     }
 // }
-// DP Solu
+// DP Solu -> Recursive + Memoization
+// class Solution {
+//     public int fib(int n) {
+//         int[] dp = new int[n+1];
+//         return helper(n,dp);
+//     }
+//     public int helper(int n,int[] dp){
+//         if(n<=1) return n;
+//         if(dp[n]!=0) return dp[n]; // check if we had ans return ans 
+//         int ans = helper(n-1,dp)+helper(n-2,dp);
+//         dp[n] = ans; // if not then calculate and store
+//         return ans;
+//     }
+// }
+// DP Solu -> Recursive + Memoization
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        return helper(n,dp);
-    }
-    public int helper(int n,int[] dp){
         if(n<=1) return n;
-        if(dp[n]!=0) return dp[n]; // check if we had ans return ans 
-        int ans = helper(n-1,dp)+helper(n-2,dp);
-        dp[n] = ans; // if not then calculate and store
-        return ans;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
 // // Recursive solu;
