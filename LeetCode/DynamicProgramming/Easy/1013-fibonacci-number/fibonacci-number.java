6c7c1712649298f07579eedbca2d38325f3ce6aa
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-22 22:11:40 +0530

    Time: 9 ms (42.80%) | Memory: 42.3 MB (14.19%) - LeetSync

--- LeetCode/DynamicProgramming/Easy/1013-fibonacci-number/fibonacci-number.java
+++ LeetCode/DynamicProgramming/Easy/1013-fibonacci-number/fibonacci-number.java
@@ -13,18 +13,26 @@
 //         return cur;
 //     }
 // }
-// Recursive Solu
+// // DP Solu
+// class Solution {
+//     static int[] dp;
+//     public int fib(int n) {
+//         dp = new int[n+1];
+//         return helper(n);
+//     }
+//     public int helper(int n){
+//         if(n<=1) return n;
+//         if(dp[n]!=0) return dp[n]; // check if we had ans or not
+//         int ans = helper(n-1)+helper(n-2);
+//         dp[n] = ans;
+//         return ans;
+//     }
+// }
+// Recursive solu;
 class Solution {
-    static int[] dp;
     public int fib(int n) {
-        dp = new int[n+1];
-        return helper(n);
-    }
-    public int helper(int n){
         if(n<=1) return n;
-        if(dp[n]!=0) return dp[n]; // check if we had ans or not
-        int ans = helper(n-1)+helper(n-2);
-        dp[n] = ans;
+        int ans = fib(n-1)+fib(n-2);
         return ans;
     }
 }
