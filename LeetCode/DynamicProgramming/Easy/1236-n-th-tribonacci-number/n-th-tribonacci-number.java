b5d758f13a648826daebeea508f371e0ae4a831d
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-23 00:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (13.50%) - LeetSync

--- LeetCode/DynamicProgramming/Easy/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ LeetCode/DynamicProgramming/Easy/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,13 +1,28 @@
-class Solution {
+// class Solution { // Recursion + Memoization
+//     public int tribonacci(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return helper(n,dp);
+//     }
+//     public int helper(int n,int[] dp){
+//         if(n<=1) return n;
+//         if(n==2) return 1;
+//         if(dp[n]!=-1) return dp[n];
+//         return dp[n] = helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
+//     }
+// }
+class Solution { // Iteration + Memoization = Tabulation
     public int tribonacci(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
-    }
-    public int helper(int n,int[] dp){
         if(n<=1) return n;
         if(n==2) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3 ; i <= n ; i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
     }
 }
