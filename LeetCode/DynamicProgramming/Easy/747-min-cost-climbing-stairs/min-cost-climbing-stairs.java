aa8e93bbbb16b3a1f1d2858fc9d47c890b6870ea
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-23 00:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (39.52%) - LeetSync

--- LeetCode/DynamicProgramming/Easy/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ LeetCode/DynamicProgramming/Easy/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,12 +1,26 @@
-class Solution {
+// class Solution { // Recursion + Memoization
+//     public int minCostClimbingStairs(int[] cost) {
+//         int[] dp = new int[cost.length];
+//         Arrays.fill(dp,-1);
+//         return Math.min(helper(0,cost,dp),helper(1,cost,dp));
+//     }
+//     public int helper(int idx,int[] cost,int[] dp){
+//         if(idx==cost.length-1||idx==cost.length-2) return cost[idx];
+//         if(dp[idx]!=-1) return dp[idx];
+//         return dp[idx] = cost[idx]+Math.min(helper(idx+1,cost,dp),helper(idx+2,cost,dp));
+//     }
+// }
+class Solution { // Tabulation
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
+        int n = cost.length;
+        int[] dp = new int[n];
         Arrays.fill(dp,-1);
-        return Math.min(helper(0,cost,dp),helper(1,cost,dp));
-    }
-    public int helper(int idx,int[] cost,int[] dp){
-        if(idx==cost.length-1||idx==cost.length-2) return cost[idx];
-        if(dp[idx]!=-1) return dp[idx];
-        return dp[idx] = cost[idx]+Math.min(helper(idx+1,cost,dp),helper(idx+2,cost,dp));
+        dp[n-1] = cost[n-1]; // base case
+        dp[n-2] = cost[n-2]; // base case
+
+        for(int i = n-3; i >=0 ;i--){
+            dp[i] = cost[i]+Math.min(dp[i+1],dp[i+2]);
+        }
+        return Math.min(dp[0],dp[1]);
     }
 }
