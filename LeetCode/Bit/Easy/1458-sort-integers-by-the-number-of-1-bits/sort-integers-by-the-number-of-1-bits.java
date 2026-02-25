a842dc008c13108575dab0e8536a7d0c6a4b84e0
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-02-25 17:46:58 +0530

    Time: 8 ms (61.40%) | Memory: 46.9 MB (27.94%) - LeetSync

--- LeetCode/Bit/Easy/1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ LeetCode/Bit/Easy/1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,16 +1,47 @@
+// class Solution { // -> Brute Force
+//     public int[] sortByBits(int[] arr) {
+//         int n = arr.length;
+//         Arrays.sort(arr);
+//         for(int i = 0; i < n-1;i++){
+//             for(int j = 0 ; j < n-i-1;j++){
+//                 if(Integer.bitCount(arr[j])>Integer.bitCount(arr[j+1])){
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// }
+// class Solution { // -> wrong because custom sorting doesnot support primitve data type
+//     public int[] sortByBits(int[] arr) {
+//         Arrays.sort(arr,(a,b)->{
+//             int countA = Integer.bitCount(a);
+//             int countB = Integer.bitCount(b);
+//             if(countA==countB) return a - b;
+//             return countA - countB;
+//         });
+//         return arr;
+//     }
+// }
 class Solution {
     public int[] sortByBits(int[] arr) {
-        int n = arr.length;
-        Arrays.sort(arr);
-        for(int i = 0; i < n-1;i++){
-            for(int j = 0 ; j < n-i-1;j++){
-                if(Integer.bitCount(arr[j])>Integer.bitCount(arr[j+1])){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
+        Integer[] temp = new Integer[arr.length];
+        
+        for(int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
         }
+        Arrays.sort(temp,(a,b)->{
+            int countA = Integer.bitCount(a);
+            int countB = Integer.bitCount(b);
+            if(countA==countB) return a - b;
+            return countA - countB;
+        });
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+
         return arr;
     }
 }
