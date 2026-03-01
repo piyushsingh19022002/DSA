c3eae7214dafd99cee44b373957ced13639b9d3e
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-01 07:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (40.92%) - LeetSync

---
+++ LeetCode/DynamicProgramming/Medium/213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+        return Math.max(helper(1,n-1,nums,dp1),helper(0,n-2,nums,dp2));
+    }
+    public int helper(int start,int end,int[] nums,int[] dp){
+        if(start>end) return 0;
+        if(dp[start]!=-1) return dp[start];
+        int take = nums[start] + helper(start+2,end,nums,dp);
+        int skip = helper(start+1,end,nums,dp);
+        return dp[start] = Math.max(take,skip);
+    }
+}
