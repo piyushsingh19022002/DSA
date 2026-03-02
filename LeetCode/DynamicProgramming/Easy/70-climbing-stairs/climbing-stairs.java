4bb3b40f70e9b1b73d8399cd890767e809edef63
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-02 23:18:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.37%) - LeetSync

--- LeetCode/DynamicProgramming/Easy/70-climbing-stairs/climbing-stairs.java
+++ LeetCode/DynamicProgramming/Easy/70-climbing-stairs/climbing-stairs.java
@@ -9,16 +9,28 @@
 //     }
 // }
 
-class Solution {
+// class Solution { // top - down
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return helper(0,n,dp);
+//     }
+//     public int helper(int curr,int n,int[] dp){
+//         if(curr>n) return 0;
+//         if(curr==n) return 1;
+//         if(dp[curr]!=-1) return dp[curr];
+//         return dp[curr] = helper(curr+1,n,dp) + helper(curr+2,n,dp);
+//     }
+// }
+
+class Solution { // top - down
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(0,n,dp);
-    }
-    public int helper(int curr,int n,int[] dp){
-        if(curr>n) return 0;
-        if(curr==n) return 1;
-        if(dp[curr]!=-1) return dp[curr];
-        return dp[curr] = helper(curr+1,n,dp) + helper(curr+2,n,dp);
+        int[] dp = new int[n+2];
+        dp[n+1] = 0;
+        dp[n] = 1;
+        for(int i = n-1 ; i >=0 ;i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
     }
 }
