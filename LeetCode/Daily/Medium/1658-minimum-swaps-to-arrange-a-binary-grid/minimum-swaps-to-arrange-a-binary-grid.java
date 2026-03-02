2392091ec3ba3db1e8df0ee170b1f5be9631c6c5
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-02 10:59:06 +0530

    Time: 2 ms (96.23%) | Memory: 50 MB (22.64%) - LeetSync

---
+++ LeetCode/Daily/Medium/1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void swap(int[] arr,int i ,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public int minSwaps(int[][] grid) {
+        int totalSwaps = 0;
+        int n = grid[0].length;
+        int[] endZeros = new int[n];
+        for(int i = 0; i < n; i++){
+    int zeroCount = 0;
+    boolean foundOne = false;
+
+    for(int j = n-1; j >= 0; j--){
+        if(grid[i][j] == 0) zeroCount++;
+        else{
+            foundOne = true;
+            break;
+        }
+    }
+
+    endZeros[i] = zeroCount; // works for both cases
+}
+        for(int i = 0 ; i < n;i++){
+            int zerosRequired = n - i - 1; // zeros required for current row
+            if(endZeros[i]>=zerosRequired) continue;
+            else{
+                int targetPos = -1;
+                for(int j = i+1 ; j <n;j++){
+                    if(endZeros[j]>=zerosRequired){
+                        targetPos = j;
+                        totalSwaps+=(j-i);
+                        break;
+                    }
+                }
+                if(targetPos==-1) return -1;
+                for(int k = targetPos;k>i;k--){
+                    swap(endZeros,k,k-1);
+                }
+            }
+        }
+        return totalSwaps;
+    }
+}
