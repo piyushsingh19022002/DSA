f3bd0ba413bf802c949bf960364210e04db8e1f0
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-02 23:48:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (27.14%) - LeetSync

--- LeetCode/DynamicProgramming/Medium/62-unique-paths/unique-paths.java
+++ LeetCode/DynamicProgramming/Medium/62-unique-paths/unique-paths.java
@@ -2,7 +2,9 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int i = 0 ; i < m ; i++){
-            Arrays.fill(dp[i],-1);
+            for(int j = 0 ; j <n ;j++){
+                dp[i][j] = -1;
+            }
         }
         return helper(0,0,m,n,dp);
     }
