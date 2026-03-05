e2cdf880c0f2d465111366400435a9f8238be38b
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-05 09:14:50 +0530

    Time: 4 ms (63.07%) | Memory: 50.1 MB (84.74%) - LeetSync

--- LeetCode/DynamicProgramming/Medium/64-minimum-path-sum/minimum-path-sum.java
+++ LeetCode/DynamicProgramming/Medium/64-minimum-path-sum/minimum-path-sum.java
@@ -10,8 +10,8 @@ class Solution {
          for(int j = 1;j<n;j++){
             dp[0][j] = grid[0][j]+dp[0][j-1];
         }
-        for(int i = 1 ; i <m;i++){
-            for(int j = 1 ; j<n;j++){
+        for(int i = 0 ; i <m;i++){
+            for(int j = 0 ; j<n;j++){
                 if(i!=0 && j!=0){
                     int val = Math.min(dp[i-1][j],dp[i][j-1]);
                     dp[i][j] = grid[i][j]+val;
