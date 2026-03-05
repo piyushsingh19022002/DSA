8202ba0f4c8122078974b80e1771415deec38dd8
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-05 10:59:52 +0530

    Time: 7 ms (49.12%) | Memory: 62.1 MB (27.29%) - LeetSync

--- LeetCode/DynamicProgramming/Medium/1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ LeetCode/DynamicProgramming/Medium/1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -5,20 +5,14 @@ class Solution {
         int count = 0;
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(i==0 && j==0) count = matrix[0][0];
-                else if(i==0 && matrix[i][j]==1){
-                    count += matrix[i][j];
-                }else if(j==0 && matrix[i][j]==1){
-                    count += matrix[i][j];
-                }else{
-                    if(matrix[i][j]!=0){
-                        int up = matrix[i-1][j];
-                        int left = matrix[i][j-1];
-                        int dia = matrix[i-1][j-1];
-                        matrix[i][j] = Math.min(up,Math.min(left,dia))+1;
-                        count += matrix[i][j];
-                    }
+                if(matrix[i][j]==0)continue;
+                if(i!=0 && j!=0){
+                    int up = matrix[i-1][j];
+                    int left = matrix[i][j-1];
+                    int dia = matrix[i-1][j-1];
+                    matrix[i][j] = Math.min(up,Math.min(left,dia))+1;
                 }
+                count += matrix[i][j];
             }
         }
         return count;
