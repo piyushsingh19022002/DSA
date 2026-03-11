5b0e36b6acd0b4774da5b2e0919a984bdbad6f61
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-11 22:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (32.44%) - LeetSync

--- LeetCode/Daily/Easy/1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ LeetCode/Daily/Easy/1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,10 +1,23 @@
+// class Solution { // Method 1 - brute force
+//     public int bitwiseComplement(int n) {
+//         StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
+//         for(int i = 0; i<sb.length();i++){
+//             if(sb.charAt(i)=='1') sb.setCharAt(i,'0');
+//             else sb.setCharAt(i,'1');
+//         }
+//         return Integer.parseInt(sb.toString(),2);
+//     }
+// }
 class Solution {
     public int bitwiseComplement(int n) {
-        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
-        for(int i = 0; i<sb.length();i++){
-            if(sb.charAt(i)=='1') sb.setCharAt(i,'0');
-            else sb.setCharAt(i,'1');
+        if(n==0) return 1;
+        int counter = 0;
+        int ans = 0;
+        while(n!=0){
+            int msb = n%2;
+            ans += Math.pow(2,counter++) *(msb==1?0:1);
+            n/=2;
         }
-        return Integer.parseInt(sb.toString(),2);
+        return ans;
     }
 }
