535ad3a12f848fa62f4ddef99c0f2d0b72dbacd0
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-03-11 22:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (91.45%) - LeetSync

--- LeetCode/Daily/Easy/1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ LeetCode/Daily/Easy/1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -8,16 +8,24 @@
 //         return Integer.parseInt(sb.toString(),2);
 //     }
 // }
+// class Solution { // Method 2 - better using log n TC
+//     public int bitwiseComplement(int n) {
+//         if(n==0) return 1;
+//         int counter = 0;
+//         int ans = 0;
+//         while(n!=0){
+//             int msb = n%2;
+//             ans += Math.pow(2,counter++) *(msb==1?0:1);
+//             n/=2;
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int bitwiseComplement(int n) {
         if(n==0) return 1;
-        int counter = 0;
-        int ans = 0;
-        while(n!=0){
-            int msb = n%2;
-            ans += Math.pow(2,counter++) *(msb==1?0:1);
-            n/=2;
-        }
-        return ans;
+        int noOfBits = (int)Math.floor(Math.log(n)/Math.log(2))+1;
+        int masked = (int)Math.pow(2,noOfBits)-1;
+        return n ^ masked;
     }
 }
