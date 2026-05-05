c62bb63646bdcaf4535c28f2631a7e7f8f256808
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-05-05 09:44:28 +0530

    Time: 32 ms (78.96%) | Memory: 59.3 MB (45.74%) - LeetSync

--- LeetCode/Arrays/TwoPointers/15-3sum/3sum.java
+++ LeetCode/Arrays/TwoPointers/15-3sum/3sum.java
@@ -17,25 +17,56 @@
 //         return new ArrayList<>(set);
 //     }
 // }
-class Solution {  //Better
+// class Solution {  //Better
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         int n = nums.length;
+//         HashSet<List<Integer>> set1 = new HashSet<>();
+//         HashSet<Integer> set2 = new HashSet<>();
+
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = i+1 ; j < n ; j++){
+//                 int rem = -1*(nums[i]+nums[j]);
+//                 if(set2.contains(rem)){
+//                     List<Integer> curr = Arrays.asList(nums[i],nums[j],rem);
+//                     Collections.sort(curr);
+//                     set1.add(curr);
+//                 }else{
+//                     set2.add(nums[j]);
+//                 }
+//             }
+//             set2.clear();
+//         }
+//         return new ArrayList<>(set1);
+//     }
+// }
+
+class Solution {  //Optimal->Using 2 pointer
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        HashSet<List<Integer>> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-
+        List<List<Integer>> ans = new ArrayList<>();
+        //Sort first
+        Arrays.sort(nums);
         for(int i = 0 ; i < n ; i++){
-            for(int j = i+1 ; j < n ; j++){
-                int rem = -1*(nums[i]+nums[j]);
-                if(set2.contains(rem)){
-                    List<Integer> curr = Arrays.asList(nums[i],nums[j],rem);
-                    Collections.sort(curr);
-                    set1.add(curr);
+            // skip if similar to prev value
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            // else proceed
+            int j = i + 1;
+            int k = n - 1;
+            // now check triplet in range i to k
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
                 }else{
-                    set2.add(nums[j]);
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;k--;
+                    while(j<k && nums[j]==nums[j-1]) j++; // increase until different
+                    while(j<k && nums[k]==nums[k+1]) k--; // decrease until different
                 }
             }
-            set2.clear();
         }
-        return new ArrayList<>(set1);
+        return ans;
     }
 }
