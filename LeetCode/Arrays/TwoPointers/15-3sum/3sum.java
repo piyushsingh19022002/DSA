9f444319fcb049a84190c6146be098d106e026e2
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-05-05 07:35:56 +0530

    Time: 2326 ms (5.00%) | Memory: 55.2 MB (98.82%) - LeetSync

---
+++ LeetCode/Arrays/TwoPointers/15-3sum/3sum.java
@@ -0,0 +1,41 @@
+// class Solution { // Brute Force
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         int n = nums.length;
+//         HashSet<List<Integer>> set = new HashSet<>();
+
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = i+1 ; j < n ; j++){
+//                 for(int k = j+1 ; k < n ; k++){
+//                     if(nums[i] + nums[j] + nums[k] == 0 && i != j && i != k && j != k){
+//                         List<Integer> curr = Arrays.asList(nums[i],nums[j],nums[k]);
+//                         Collections.sort(curr);
+//                         set.add(curr);
+//                     } 
+//                 }
+//             }
+//         }
+//         return new ArrayList<>(set);
+//     }
+// }
+class Solution {  //Better
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        HashSet<List<Integer>> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                int rem = -1*(nums[i]+nums[j]);
+                if(set2.contains(rem)){
+                    List<Integer> curr = Arrays.asList(nums[i],nums[j],rem);
+                    Collections.sort(curr);
+                    set1.add(curr);
+                }else{
+                    set2.add(nums[j]);
+                }
+            }
+            set2.clear();
+        }
+        return new ArrayList<>(set1);
+    }
+}
