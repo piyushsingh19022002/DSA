89d651a48ed4f6c3ccb252a09d95e0c477abf2be
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-05-05 10:30:31 +0530

    Time: 15 ms (98.10%) | Memory: 45.8 MB (14.60%) - LeetSync

--- LeetCode/Arrays/TwoPointers/16-3sum-closest/3sum-closest.java
+++ LeetCode/Arrays/TwoPointers/16-3sum-closest/3sum-closest.java
@@ -1,20 +1,50 @@
-class Solution {
+// class Solution { // Brute Force
+//     public int threeSumClosest(int[] nums, int target) {
+//         int n = nums.length;
+//         int closest = Integer.MAX_VALUE;
+//         int ans = 0;
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = i + 1 ; j < n ; j++){
+//                 for(int k = j + 1 ; k < n ; k++){
+//                     int sum = nums[i]+nums[j]+nums[k];
+//                     int diff = Math.abs(sum-target);
+//                     if(closest>diff){
+//                         closest = diff;
+//                         ans = sum;
+//                     } 
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+class Solution { // Optimal 
     public int threeSumClosest(int[] nums, int target) {
         int n = nums.length;
-        int closest = Integer.MAX_VALUE;
-        int ans = 0;
+        int diff = Integer.MAX_VALUE;
+        int sum = 0;
+        Arrays.sort(nums);
         for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                for(int k = j + 1 ; k < n ; k++){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    int diff = Math.abs(sum-target);
-                    if(closest>diff){
-                        closest = diff;
-                        ans = sum;
-                    } 
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            while(j<k){
+                int csum = nums[i]+nums[j]+nums[k];
+                int cdiff = Math.abs(target-csum);
+
+                if(cdiff<diff){
+                    diff = cdiff;
+                    sum = csum;
                 }
+
+                if(csum>target)k--;
+                else if(csum<target)j++;
+                else return csum;
             }
         }
-        return ans;
+        return sum;
     }
 }
