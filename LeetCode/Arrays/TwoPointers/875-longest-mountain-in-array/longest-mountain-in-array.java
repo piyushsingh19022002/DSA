7edef729bb801d703868b792b6525bc2da203c8e
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-05-13 08:14:58 +0530

    Time: 130 ms (5.08%) | Memory: 47.8 MB (6.79%) - LeetSync

--- LeetCode/Arrays/TwoPointers/875-longest-mountain-in-array/longest-mountain-in-array.java
+++ LeetCode/Arrays/TwoPointers/875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,17 +1,42 @@
-class Solution {
+class Solution { // Brute Force Approach
     public int longestMountain(int[] arr) {
         int n = arr.length;
-        if(n<3) return 0;
-        int maxL = 0;
-        for(int i = 1 ; i < n-1 ; i++){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                int start = i;
-                int end = i;
-                while(start>=1 && arr[start]>arr[start-1]) start--;
-                while(end<n-1 && arr[end]>arr[end+1]) end++;
-                maxL = Math.max(end-start+1,maxL);
-            };
+        int ans = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            int incre = 0;
+            int decre = 0;
+            int j = i + 1;
+
+            while(j<n && arr[j]>arr[j-1]){
+                incre = 1;
+                j++;
+            }
+
+            while(j<n && arr[j]<arr[j-1]){
+                decre = 1;
+                j++;
+            }
+
+            if(incre==1 && decre==1) ans = Math.max(ans,j-i);
         }
-        return maxL;
+        return ans;
     }
 }
+// class Solution { // Better Approach
+//     public int longestMountain(int[] arr) {
+//         int n = arr.length;
+//         if(n<3) return 0;
+//         int maxL = 0;
+//         for(int i = 1 ; i < n-1 ; i++){
+//             if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+//                 int start = i;
+//                 int end = i;
+//                 while(start>=1 && arr[start]>arr[start-1]) start--;
+//                 while(end<n-1 && arr[end]>arr[end+1]) end++;
+//                 maxL = Math.max(end-start+1,maxL);
+//             };
+//         }
+//         return maxL;
+//     }
+// }
