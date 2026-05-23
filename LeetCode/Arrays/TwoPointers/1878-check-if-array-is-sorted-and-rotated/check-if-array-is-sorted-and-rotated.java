33223b4fff149b4519fe033fd01e99a06868bcfd
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-05-23 11:33:06 +0530

    Time: 1 ms (5.67%) | Memory: 42.9 MB (62.78%) - LeetSync

---
+++ LeetCode/Arrays/TwoPointers/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,52 @@
+// class Solution {
+//     public boolean check(int[] nums) {
+//         int len = nums.length;
+//         int deviation = 0;
+
+//         for(int i = 0 ; i < len ; i++){
+//             if(i < len-1 && nums[i]>nums[i+1]){
+//                 deviation++;
+//             }else if(i == len-1 && nums[len-1]>nums[0]){
+//                 deviation++;
+//             }
+//         }
+
+//         if(deviation>1) return false;
+//         else return true;
+        
+//     }
+// }
+
+class Solution { // Brute Force
+    public void rotateByOne(int[] nums){
+        int i = 1,j = nums.length-1;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;j--;
+        }
+        i = 0;j = nums.length-1;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;j--;
+        }
+    }
+    public boolean isSorted(int[] nums){
+        for(int i = 0 ; i < nums.length-1 ;i++){
+            if(nums[i]>nums[i+1]) return false;
+        }
+        return true;
+    }
+    public boolean check(int[] nums) {
+        int len = nums.length;
+        if(len<3) return true;
+        for(int i = 0 ; i < len ; i++){
+            rotateByOne(nums);
+            if(isSorted(nums)) return true;
+        }
+        return false;
+    }
+}
