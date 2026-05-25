0aa041ba742f559000b1257ae0ccdaa759245d31
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-05-25 08:02:18 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (74.38%) - LeetSync

--- LeetCode/Arrays/TwoPointers/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ LeetCode/Arrays/TwoPointers/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,14 +1,35 @@
-class Solution {
+// class Solution { // brute force
+//     public int[] sortedSquares(int[] nums) {
+//         int n = nums.length;
+//         int[] sq = new int[n];
+//         // making original array sort a/c absolute val
+//         for(int i = 0 ; i < n ; i++){
+//             nums[i] = Math.abs(nums[i]);
+//         }
+//         Arrays.sort(nums);
+//         // square it and store one by one
+//         for(int i = 0 ; i < n ; i++){
+//             sq[i] = (int)Math.pow(nums[i],2);
+//         }
+//         return sq;
+//     }
+// }
+class Solution { // optimal -> 2 pointer
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
+        int i = 0;
+        int j = n-1;
         int[] sq = new int[n];
-        // making original array sort absolute
-        for(int i = 0 ; i < n ; i++){
-            nums[i] = Math.abs(nums[i]);
-        }
-        Arrays.sort(nums);
-        for(int i = 0 ; i < n ; i++){
-            sq[i] = (int)Math.pow(nums[i],2);
+        int k = n-1;
+        while(i<=j){
+            if(Math.abs(nums[i])>Math.abs(nums[j])){
+                sq[k] = nums[i]*nums[i];
+                i++;
+            }else{
+                sq[k] = nums[j]*nums[j];
+                j--;
+            }
+            k--;
         }
         return sq;
     }
