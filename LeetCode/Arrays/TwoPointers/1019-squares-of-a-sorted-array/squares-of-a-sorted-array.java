d41805f6e7cd8951fcfc5c84fa84589c22006f1b
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-07-08 16:35:04 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (76.31%) - LeetSync

--- LeetCode/Arrays/TwoPointers/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ LeetCode/Arrays/TwoPointers/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,36 +1,25 @@
-// class Solution { // brute force
-//     public int[] sortedSquares(int[] nums) {
-//         int n = nums.length;
-//         int[] sq = new int[n];
-//         // making original array sort a/c absolute val
-//         for(int i = 0 ; i < n ; i++){
-//             nums[i] = Math.abs(nums[i]);
-//         }
-//         Arrays.sort(nums);
-//         // square it and store one by one
-//         for(int i = 0 ; i < n ; i++){
-//             sq[i] = (int)Math.pow(nums[i],2);
-//         }
-//         return sq;
-//     }
-// }
-class Solution { // optimal -> 2 pointer
+class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
-        int i = 0;
-        int j = n-1;
-        int[] sq = new int[n];
-        int k = n-1;
-        while(i<=j){
-            if(Math.abs(nums[i])>Math.abs(nums[j])){
-                sq[k] = nums[i]*nums[i];
-                i++;
+        int[] square = new int[n];
+
+        int low = 0;
+        int high = n-1;
+        int writePos = n-1;
+
+        while(low<=high){
+            int sql = nums[low]*nums[low];
+            int sqh = nums[high]*nums[high];
+
+            if(sql>sqh){
+                square[writePos] = sql;
+                low++;
             }else{
-                sq[k] = nums[j]*nums[j];
-                j--;
+                square[writePos] = sqh;
+                high--;
             }
-            k--;
+            writePos--;
         }
-        return sq;
+        return square;
     }
 }
