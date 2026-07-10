e4a1ae0e95bdb5239b3f5098792e7c4fc51ff0ff
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-07-10 17:27:54 +0530

    Time: 3 ms (68.67%) | Memory: 47.5 MB (18.40%) - LeetSync

--- LeetCode/Arrays/TwoPointers/875-longest-mountain-in-array/longest-mountain-in-array.java
+++ LeetCode/Arrays/TwoPointers/875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,42 +1,26 @@
-class Solution { // Brute Force Approach
+class Solution {
     public int longestMountain(int[] arr) {
         int n = arr.length;
-        int ans = 0;
-        
-        for(int i = 0 ; i < n ; i++){
-            int incre = 0;
-            int decre = 0;
-            int j = i + 1;
+        int maxRange = 0;
+        if(n<3) return maxRange;
 
-            while(j<n && arr[j]>arr[j-1]){
-                incre = 1;
-                j++;
-            }
+        int i = 1;
 
-            while(j<n && arr[j]<arr[j-1]){
-                decre = 1;
-                j++;
-            }
+        while(i<n-1){
+            boolean isPeak = arr[i]>arr[i-1] && arr[i]>arr[i+1];
 
-            if(incre==1 && decre==1) ans = Math.max(ans,j-i);
+            if(isPeak){
+                int start = i;
+                int end = i;
+
+                while(start>0 && arr[start]>arr[start-1]) start--;
+                while(end<n-1 && arr[end]>arr[end+1]) end++;
+
+                maxRange = Math.max(maxRange,end-start+1);
+
+                i = end;
+            }else i++;
         }
-        return ans;
+        return maxRange;
     }
 }
-// class Solution { // Better Approach
-//     public int longestMountain(int[] arr) {
-//         int n = arr.length;
-//         if(n<3) return 0;
-//         int maxL = 0;
-//         for(int i = 1 ; i < n-1 ; i++){
-//             if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-//                 int start = i;
-//                 int end = i;
-//                 while(start>=1 && arr[start]>arr[start-1]) start--;
-//                 while(end<n-1 && arr[end]>arr[end+1]) end++;
-//                 maxL = Math.max(end-start+1,maxL);
-//             };
-//         }
-//         return maxL;
-//     }
-// }
