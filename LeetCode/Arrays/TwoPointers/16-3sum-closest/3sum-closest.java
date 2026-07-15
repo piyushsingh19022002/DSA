4deaa240fe44f2a7023a2cf1f4c7ea4a69949b18
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-07-15 10:48:45 +0530

    Time: 18 ms (41.73%) | Memory: 45.7 MB (31.16%) - LeetSync

--- LeetCode/Arrays/TwoPointers/16-3sum-closest/3sum-closest.java
+++ LeetCode/Arrays/TwoPointers/16-3sum-closest/3sum-closest.java
@@ -1,50 +1,21 @@
-// class Solution { // Brute Force
-//     public int threeSumClosest(int[] nums, int target) {
-//         int n = nums.length;
-//         int closest = Integer.MAX_VALUE;
-//         int ans = 0;
-//         for(int i = 0 ; i < n ; i++){
-//             for(int j = i + 1 ; j < n ; j++){
-//                 for(int k = j + 1 ; k < n ; k++){
-//                     int sum = nums[i]+nums[j]+nums[k];
-//                     int diff = Math.abs(sum-target);
-//                     if(closest>diff){
-//                         closest = diff;
-//                         ans = sum;
-//                     } 
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// }
-
-
-
-class Solution { // Optimal Approach
+class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int n = nums.length;
-        int diff = Integer.MAX_VALUE;
-        int sum = 0;
+        int closest = nums[0] + nums[1] + nums[2];
         Arrays.sort(nums);
+
         for(int i = 0 ; i < n ; i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j = i + 1;
-            int k = n - 1;
-            while(j<k){
-                int csum = nums[i]+nums[j]+nums[k];
-                int cdiff = Math.abs(target-csum);
+            int left = i + 1;
+            int right = n - 1;
 
-                if(cdiff<diff){
-                    diff = cdiff;
-                    sum = csum;
-                }
-
-                if(csum>target)k--;
-                else if(csum<target)j++;
-                else return csum;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(Math.abs(sum-target)<Math.abs(closest-target))closest = sum;
+                if(sum==target) return sum;
+                else if(sum>target) right--;
+                else left++;
             }
         }
-        return sum;
+        return closest;
     }
 }
