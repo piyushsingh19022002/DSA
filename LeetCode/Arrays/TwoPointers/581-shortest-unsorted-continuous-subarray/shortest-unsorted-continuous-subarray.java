ac4544b622a3e5b22d6c3e2961e7e14984e825d3
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-07-15 11:49:19 +0530

    Time: 1 ms (90.48%) | Memory: 47.1 MB (68.10%) - LeetSync

--- LeetCode/Arrays/TwoPointers/581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ LeetCode/Arrays/TwoPointers/581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -1,64 +1,27 @@
-// class Solution { 
-//     public int findUnsortedSubarray(int[] nums) {
-//         int n = nums.length;
-//         int[] cpy = nums.clone();
-//         Arrays.sort(cpy);
-//         int i = 0;
-//         int j = n - 1;
-//         while(i<j && nums[i]==cpy[i])i++;
-//         while(i<j && nums[j]==cpy[j])j--;
-//         return i==j?0:j-i+1;
-//     }
-// }
-// class Solution {
-//     public int findUnsortedSubarray(int[] nums) {
-//         int n = nums.length;
-//         if(n==1) return 0;
-//         int i = 0; 
-//         int j = n-1;
-
-//         while(i<j){
-//             if(nums[i]>nums[i+1] && nums[j]<nums[j-1]){
-//                 break;
-//             }else if(nums[i]<=nums[i+1]){
-//                 i++;
-//             }else if(nums[j]>=nums[j-1]){
-//                 j--;
-//             }else{
-//                 continue;
-//             }
-//         }
-//         return i==j?0:j-i+1;
-//     }
-// }
-
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int n = nums.length;
-        int min = Integer.MAX_VALUE;
-        int st = -1;
-        int max = Integer.MIN_VALUE;
+        int start = -1;
+        int maxSofar = nums[0];
         int end = -1;
+        int minSofar = nums[n-1];
 
-        // finding the smallest element which is unsorted
-        for(int i = 1 ; i < n; i++){
-            if(nums[i]<nums[i-1] && st==-1){
-                min = nums[i];
-                st = i;
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i]<maxSofar){
+                end = i;
+            }else{
+                maxSofar = nums[i];
             }
-            if(st!=-1) min = Math.min(min,nums[i]);
         }
-        if(st==-1) return 0;
-        for(int j = n-2 ; j >=0 ;j--){
-            if(nums[j]>nums[j+1] && end==-1){
-                max = nums[j];
-                end = j;
+
+        for(int i = n - 2 ; i>=0 ;i--){
+            if(nums[i]>minSofar){
+                start = i;
+            }else{
+                minSofar = nums[i];
             }
-            if(end!=-1) max = Math.max(max,nums[j]);
         }
-        st = 0 ;end = n-1;
-        while(st<n && nums[st]<=min) st++;
-        while(end>=0 && nums[end]>=max) end--;
-        return end-st+1;
+        if(start==-1 && end==-1) return 0;
+        return end - start + 1;
     }
 }
