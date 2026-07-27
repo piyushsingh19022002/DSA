3aff7cf44171a46ebe7c314ce3c99ff8296eccb7
Piyush Singh <143880809+piyushsingh19022002@users.noreply.github.com>
2026-07-27 23:30:04 +0530

    Time: 6 ms (99.35%) | Memory: 49.7 MB (58.92%) - LeetSync

--- LeetCode/Arrays/TwoPointers/49-group-anagrams/group-anagrams.java
+++ LeetCode/Arrays/TwoPointers/49-group-anagrams/group-anagrams.java
@@ -1,16 +1,11 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         int n = strs.length;
-        HashMap<String , ArrayList<String>> map = new HashMap<>();
-
+        HashMap<String,List<String>> map = new HashMap<>();
         for(int i = 0 ; i < n ; i++){
-            // sorting ele of arr
             char[] arr = strs[i].toCharArray();
             Arrays.sort(arr);
             String key = new String(arr);
-
-            // creating list or adding
-
             if(!map.containsKey(key)){
                 map.put(key,new ArrayList<>());
             }
